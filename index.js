510415c8255ae41e0a804da9987cdba800af5c4a
github username <rbh0005@gmail.com>
2018-11-21 12:47:16 -0800

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,63 @@
-function produceDrivingRange(range) {
-  return function (beginning, end) {
-    let distance = Math.abs(Number(end.slice(0,2)) - Number(beginning.slice(0,2)))
-    if (range > distance) {
-      return `within range by ${range - distance}`
+function produceDrivingRange(blockRange) {
+  return function(first, second) {
+    first = parseInt(first);
+    second = parseInt(second);
+    distance = second - first;
+    if (distance > blockRange) {
+      return `${distance - blockRange} blocks out of range`
     } else {
-      return `${distance - range} blocks out of range`
+      return `within range by ${blockRange - distance}`
     }
   }
 }
 
-function produceTipCalculator(tipPercent) {
-  return function (fare) {
-    return fare * tipPercent
+function produceTipCalculator(tip) {
+  return function(fare) {
+    return tip * fare
   }
 }
+
+function createDriver() {
+  let driverId = 0;
+  return class Driver {
+    constructor(name) {
+      this.name = name;
+      this.id = ++driverId;
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function produceDrivingRange(range) {
+//   return function (beginning, end) {
+//     let distance = Math.abs(Number(end.slice(0,2)) - Number(beginning.slice(0,2)))
+//     if (range > distance) {
+//       return `within range by ${range - distance}`
+//     } else {
+//       return `${distance - range} blocks out of range`
+//     }
+//   }
+// }
+//
+// function produceTipCalculator(tipPercent) {
+//   return function (fare) {
+//     return fare * tipPercent
+//   }
+// }
